052138e9ab0eef601a4770cc374c7c0e3b13e5c6
arnaudkapko-arch <arnaudkapko@gmail.com>
2026-05-13 11:02:49 +0200

    Update index.ts

--- IHM/IHM_voiture/Panel_Destination/src/index.ts
+++ IHM/IHM_voiture/Panel_Destination/src/index.ts
@@ -1,11 +1,16 @@
 import { ExtensionContext } from "@foxglove/extension";
+<<<<<<< Updated upstream
 import { createRoot } from "react-dom/client";
 import React from "react";
 import DestinationPanel from "./DestinationPanel";
+=======
+import { initDestinationPanel } from "./DestinationPanel";
+>>>>>>> Stashed changes
 
 export function activate(extensionContext: ExtensionContext): void {
   extensionContext.registerPanel({
     name: "Destination",
+<<<<<<< Updated upstream
     initPanel: (context) => {
       const div = document.createElement("div");
       div.style.width  = "100%";
@@ -15,5 +20,8 @@ export function activate(extensionContext: ExtensionContext): void {
       root.render(React.createElement(DestinationPanel, { context }));
       return () => root.unmount();
     },
+=======
+    initPanel: initDestinationPanel,
+>>>>>>> Stashed changes
   });
 }
